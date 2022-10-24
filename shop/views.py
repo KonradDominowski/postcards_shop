d75efac512b7d2c8f2d87bc2accf064be2184e0d
Konrad Dominowski <zehkan@gmail.com>
2022-10-24 09:34:09 +0200

    update photo view

--- shop/views.py
+++ shop/views.py
@@ -16,7 +16,7 @@ from .functions import get_exact_info
 class Home(View):
     def get(self, request):
         count = Photo.objects.count()
-        photos = Photo.objects.all()
+        photos = Photo.objects.all().order_by('pk')
         context = {
             'count': count,
             'photos': photos
@@ -51,16 +51,6 @@ def is_duplicate(photo):
 
 
 class ConfirmPhoto(View):
-    @staticmethod
-    def is_duplicate(photo):
-        media_url = os.path.join(settings.MEDIA_ROOT, IMG_DIR)
-        all_photos = [f for f in listdir(media_url) if isfile(join(media_url, f))]
-        name, extension = (os.path.splitext(photo.name))
-        index = name[::-1].find('_')
-        slash_index = name[::-1].find('/')
-        file_name = name[-slash_index:-index - 1] + extension
-
-        return file_name in all_photos
 
     def get(self, request, photo_id):
         photo = Photo.objects.get(pk=photo_id)
@@ -75,7 +65,7 @@ class ConfirmPhoto(View):
         context = {
             'form': form,
             'photo': photo,
-            'is_duplicate': self.is_duplicate(photo)
+            'is_duplicate': is_duplicate(photo)
         }
 
         return render(request, 'confirm_photo.html', context)
@@ -94,3 +84,11 @@ class DeletePhoto(View):
         photo.delete()
         os.remove(os.path.join(settings.MEDIA_ROOT, photo.name))
         return redirect(reverse('shop-home'))
+
+
+class UpdatePhoto(UpdateView):
+    model = Photo
+    fields = '__all__'
+    template_name = 'confirm_photo.html'
+    success_url = '/'
+
